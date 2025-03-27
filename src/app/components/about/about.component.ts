0ca297a534f18f4e5fc39110c5f5d34b19d0a169
joaohdib <joaohdib@gmail.com>
2025-03-27 20:31:51 -0300

    adjusted arrow on about page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -8,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  constructor(private router: Router) {}
+
+  back() {
+    this.router.navigate(['/']);
+  }
 }
